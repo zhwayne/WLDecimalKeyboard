27dcdf95f7e406cfce33ecc5794842298bcb7a2e
Wayne <mrzhwayne@163.com>
2016-06-14 09:37:26 +0800

    删除无用属性

--- customkeyboard/customkeyboard/WLDecimalKeyboard.h
+++ customkeyboard/customkeyboard/WLDecimalKeyboard.h
@@ -14,7 +14,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (copy, nonatomic, nullable) void (^done)();       /*< 点击确定执行的回调 */
 @property (nonatomic) UIColor *tintColor;                   /*< 主色调（针对确定按钮） */
-@property (copy, nonatomic, nullable) BOOL (^shouldInput)(id<UIKeyInput> inputView);
 
 - (instancetype)initWithTintColor:(UIColor *)tintColor;
 
